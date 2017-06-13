223de42cbc80e060e5d78212300758eb9917aa50
Alexis Cachat <alexis.cachat@joventures.com>
2017-06-13 14:24:56 -0400

    removed buggy git clone.

--- tweak_vim.sh
+++ tweak_vim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# getting the repo
-cd
-git clone https://github.com/alexis3301/vim_settings.git 
 ln -s ~/vim_settings/.vimrc ~/.vimrc
 ln -s ~/vim_settings/.plugin.vim ~/.plugin.vim
 mkdir .vim && cd .vim && mkdir bundle && cd bundle
