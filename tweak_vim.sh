3b315447d171f9950c7ef271f1a3bb70ba239d88
Alexis <alexis@acweb.ca>
2017-07-20 09:08:05 -0400

    fixed issue with the bash script
     not founding .vim folder

--- tweak_vim.sh
+++ tweak_vim.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/vim_settings/.vimrc ~/.vimrc
 ln -s ~/vim_settings/.plugin.vim ~/.plugin.vim
-mkdir .vim && cd .vim && mkdir bundle && cd bundle
+cd ~ && mkdir .vim && cd .vim && mkdir bundle && cd bundle
 ln -s ~/vim_settings/snippets ~/.vim/snippets
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
